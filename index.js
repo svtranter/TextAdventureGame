ac679fea60cae6840d8a2d744e94debde998cf35
svtranter <82532670+svtranter@users.noreply.github.com>
2021-06-15 00:08:06 +0100

    Added search, fight still needs fixing

--- index.js
+++ index.js
@@ -44,16 +44,6 @@ class City {
             return this;
         }
     }
-    search() {
-        let text;
-    if (city.item === "") {
-        text = "There are no items to be found here."
-    } else {
-        text = "You have found " + city.item + "!";
-    }
-    document.getElementById("currentEvent").innerHTML = text;
-    document.getElementById("userInput").focus();   
-    }
 }
 
 class Character {
@@ -89,7 +79,7 @@ class Character {
 }
 
 let inventory = [];
-let companions = [];
+/*let companions = []; Don't need this currently as working with one Pokaymon*/
 
 class Pokaymon extends Character {
     constructor(name, description, conversation, type, weakness) {
@@ -97,35 +87,21 @@ class Pokaymon extends Character {
         this._type = type;
         this._weakness = weakness;
     }
-    addToCompanions(name) {
-        companions.append(this._name);
+    get type() {
+        return this._type;
     }
-}
-let fightStatus;
-class GymLeader extends Pokaymon {
-    constructor(name, description, conversation, type, weakness, weakness2, weakness3, badge) {
-        super(name, description, conversation, type, weakness)
-        this._weakness2 = weakness2;
-        this._weakness3 = weakness3;
-        this._badge = badge;
-        
+    set type(value) {
+        this._type = value;
     }
-    fight(badge, type) {
-        if (badge === this._weakness || badge === this._weakness2 ||badge === this._weakness3 ||  type === this._weakness || type === this._weakness2 || type === this._weakness3) {
-            fightStatus = "win";
-            return true;
-        } else {
-            fightStatus = "lose"
-            return false;
-        }
+    get weakness() {
+        return this._weakness;
     }
-    talk() {
-        if (fightStatus === "win") {
-            return this._name + " says '" + this._conversation + "'";
-        } else {
-            return this._name + " cackles loudly '" + this._conversation + "'";
-        }
+    set weakness(value) {
+        this._weakness = value;
     }
+    /*addToCompanions(name) {
+        companions.push(this._name);
+    } Don't need this at the moment, only dealing with one Pokaymon atm*/
 }
 
 class Item {
@@ -146,7 +122,7 @@ class Item {
         this._description = value;
     }
     describe() {
-        return "This is a " + this._name + ", " + this._description;
+        return this._name + ", this is a " + this._description;
     }
 }
 
@@ -155,19 +131,37 @@ class Badge extends Item {
         super(name, description)
         this._type = type;
     }
-    get name() {
-        return this._name;
+    get type() {
+        return this._type;
     }
-    set name(value) {
-        this._name = value;
+    set type(value) {
+        this._type = value;
     }
-    get description() {
-        return this._description;
-    }
-    set description(value) {
-        this._description = value;
+    describe() {
+        return this._name + " " + this._description;
     }
 }
+let fightStatus;
+class GymLeader extends Pokaymon {
+    constructor(name, description, conversation, type, weakness, badge) {
+        super(name, description, conversation, type, weakness)
+        this._badge = badge;
+    }
+    get badge() {
+        return this._badge;
+    }
+    set badge(value) {
+        this._badge = value;
+    }    
+    fight() {
+        if (this._weakness === "electric" /*|| inventory.includes(this.weakness)*/) {
+            return true;
+        } else {
+            return false;
+        } 
+    }
+}
+
 
 function displayCityInfo(city) {
     let characterMsg = "";
@@ -183,12 +177,12 @@ function displayCityInfo(city) {
 
 function firstVisitCratetown() {
     const text = "You awake to find your best friend Peekachoo watching you. Peekachoo is one of the many creatures called 'Pokaymon' that inhabit this world.";
-    document.getElementById("currentEvent").innerHTML = text;
+    document.getElementById("startEvent").innerHTML = text;
     document.getElementById("userInput").focus();
 }
 
 function help() {
-    const text = "You are a Pokaymon trainer travelling the Canto region on your quest to acquire all 8 gym badges. You can use commands such as north or east to move around. You can interact with characters or objects by using 'fight' or 'inventory'.";
+    const text = "You are a Pokaymon trainer travelling the Canto region on your quest to acquire all 8 gym badges. You can use commands such as north or east to move around. You can interact with characters or objects by using 'fight', 'search' or 'inventory'.";
     document.getElementById("currentEvent").innerHTML = text;
     document.getElementById("userInput").focus();
 }
@@ -201,11 +195,38 @@ function getInventory() {
         text = inventory[0] + ", ";
         for (i = 1; i < inventory.length; i++) {
             text += inventory[i] + ", ";
-        }
-        
+        }   
     }
     document.getElementById("currentEvent").innerHTML = text;
     document.getElementById("userInput").focus();
+}
+
+function search(city) {
+    let text;
+    if (city.item === "") {
+        text = "There are no items to be found here."
+    } else {
+        text = "You have found " + city.item.describe() + "!";
+    }
+    document.getElementById("currentEvent").innerHTML = text;
+    document.getElementById("userInput").focus();   
+}
+
+function fight(GymLeader) {
+    let msg = "";
+    if (check(GymLeader.fight())) {
+        msg = "Your attack was effective! Peekachoo wins! You have gained the " + city.character.badge.describe() + "!";
+        inventory.push(this._badge);
+    } else {
+        loseGame();
+    }
+    document.getElementById("currentEvent").innerHTML = msg;
+    document.getElementById("userInput").focus();
+}
+
+function loseGame() {
+    alert("You received a critical hit and unfortunately Peekachoo fainted.")
+    /*Need to make the game start again*/
 }
 
 let currentCity;
@@ -217,6 +238,7 @@ function startGame() {
     
     document.addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
+            document.getElementById("startEvent").innerHTML = "";
             document.getElementById("currentEvent").innerHTML = "";
             const command = document.getElementById("userInput").value;
             const directions = ["east", "west", "north", "south"];
@@ -225,7 +247,11 @@ function startGame() {
                 help();
             } else if (command.toLowerCase() === "inventory") {
                 getInventory();
-            } else if (directions.includes(command.toLowerCase())) {
+            } else if (command.toLowerCase() === "search") {
+                search(currentCity);
+            } else if (command.toLowerCase() === "fight") {
+                fight(currentCity.character);
+            }else if (directions.includes(command.toLowerCase())) {
                 currentCity = currentCity.move(command.toLowerCase());
                 displayCityInfo(currentCity);
             } else {
@@ -246,27 +272,27 @@ const amber = new City("Amber City", "here you can find the psychic-type gym.");
 const olive = new City("Olive City", "here you can find the ground-type gym.");
 const blush = new City("Blush City", "here you can find the poison-type gym.");
 
-const pebble = new Badge("Pebble Badge", "gained by defeated gym leader Block", "rock");
-const waterfall = new Badge("Waterfall Badge", "gained by defeated gym leader Foggy", "water");
-const cracking = new Badge("Cracking Badge", "gained by defeated gym leader Lt. Emerge", "electric");
-const prism = new Badge("Prism Badge", "gained by defeated gym leader Ericar", "grass");
-const spirit = new Badge("Spirit Badge", "gained by defeated gym leader Toga", "poison");
-const quagmire = new Badge("Quagmire Badge", "gained by defeated gym leader Hermione", "psychic");
-const terrain = new Badge("Terrain Badge", "gained by defeated gym leader Geovanni", "ground");
-const lava = new Badge("Lava Badge", "gained by defeated gym leader Flame", "fire");
+const pebble = new Badge("Pebble Badge", "gained by defeating gym leader Block", "rock");
+const waterfall = new Badge("Waterfall Badge", "gained by defeating gym leader Foggy", "water");
+const cracking = new Badge("Cracking Badge", "gained by defeating gym leader Lt. Emerge", "electric");
+const prism = new Badge("Prism Badge", "gained by defeating gym leader Ericar", "grass");
+const spirit = new Badge("Spirit Badge", "gained by defeating gym leader Toga", "poison");
+const quagmire = new Badge("Quagmire Badge", "gained by defeating gym leader Hermione", "psychic");
+const terrain = new Badge("Terrain Badge", "gained by defeating gym leader Geovanni", "ground");
+const lava = new Badge("Lava Badge", "gained by defeating gym leader Flame", "fire");
 
 const peekachoo = new Pokaymon("Peekachoo", "is an electric-type Pokaymon", "Peeka! Peeekkaaa!", "electric", "ground");
 
 const mum = new Character("Mum", "she is always there for you and always gives great advice", "Have you tried asking for help?")
 
-const foggy = new GymLeader("Foggy", "she is the water-type gym leader", "Hi! You're a new face! What's your policy on Pokaymon? What's your approach? My policy for battle is... an all-out offensive with Water-type Pokaymon!", "water", "grass", "electric", "", waterfall);
-const ericar = new GymLeader("Ericar", "she is the grass-type gym leader", "Welcome. My name is Ericar. I am the Gym Leader of Jade City Gym. I am a student of the art of flower arranging. My Pokaymon are all of the Grass type. ...Oh, I'm sorry. Did you perhaps wish to challenge me?", "grass", "poison", "fire", "", prism);
-const ltemerge = new GymLeader("Lt. Emerge", "he is the electric-type gym leader", "Hey, kid! What do you think you're doing here? You won't live long in combat! Not with your puny power! I tell you, kid, electric Pokaymon saved me during the war! They zapped my enemies into paralysis! The same as I'll do you to you.", "electric", "ground", "", "", cracking);
-const flame = new GymLeader("Flame", "he is the fire-type gym leader", "Hah! I'm Flame, the red-hot Leader of Scarlet Gym! My fiery Pokaymon are all ready with intense heat! They incinerate all challengers!","fire", "ground", "rock", "water", lava);
-const block = new GymLeader("Block", "he is the rock-type gym leader", "I'm Block, Lead City's Gym Leader. You can see just by looking at my Pokaymon how rock hard my willpower is. My Pokaymon are all hard as rock and have true-grit determination!","rock", "water", "grass", "", pebble);
-const hermione = new GymLeader("Hermione", "she is the psychic-type gym leader", "... So you've come! I had a vision of your arrival. My Pokaymon is hurt, if you could give me a potion, I will give you my badge.", "psychic", "", "", "", quagmire);
-const geovanni = new GymLeader("Geovanni", "he is the ground-type gym leader", "So! I must say, I am impressed you got here. Team Locket captures Pokaymon from around the world. They're important tools for keeping our criminal enterprise going. I am the leader, Geovanni! For your insolence, you will feel a world of pain!", "ground", "water", "grass", "", terrain);
-const toga = new GymLeader("Toga", "he is the poison-type gym leader", "Fwahahaha! A mere child like you dares to challenge me? That very idea makes me shiver with mirth! Very well, I shall show you true terror as a ninja master! Opponents can't lay a hand on me, as poison brings their steady doom.", "poison", "ground", "psychic", "", spirit);
+const foggy = new GymLeader("Foggy", "she is the water-type gym leader", "Hi! You're a new face! What's your policy on Pokaymon? What's your approach? My policy for battle is... an all-out offensive with Water-type Pokaymon!", "water", "electric", waterfall);
+const ericar = new GymLeader("Ericar", "she is the grass-type gym leader", "Welcome. My name is Ericar. I am the Gym Leader of Jade City Gym. I am a student of the art of flower arranging. My Pokaymon are all of the Grass type. ...Oh, I'm sorry. Did you perhaps wish to challenge me?", "grass", "fire", prism);
+const ltemerge = new GymLeader("Lt. Emerge", "he is the electric-type gym leader", "Hey, kid! What do you think you're doing here? You won't live long in combat! Not with your puny power! I tell you, kid, electric Pokaymon saved me during the war! They zapped my enemies into paralysis! The same as I'll do you to you.", "electric", "ground", cracking);
+const flame = new GymLeader("Flame", "he is the fire-type gym leader", "Hah! I'm Flame, the red-hot Leader of Scarlet Gym! My fiery Pokaymon are all ready with intense heat! They incinerate all challengers!","fire", "rock", lava);
+const block = new GymLeader("Block", "he is the rock-type gym leader", "I'm Block, Lead City's Gym Leader. You can see just by looking at my Pokaymon how rock hard my willpower is. My Pokaymon are all hard as rock and have true-grit determination!","rock", "grass", pebble);
+const hermione = new GymLeader("Hermione", "she is the psychic-type gym leader", "... So you've come! I had a vision of your arrival. My Pokaymon is hurt, if you could give me a potion, I will give you my badge.", "psychic", "", quagmire);
+const geovanni = new GymLeader("Geovanni", "he is the ground-type gym leader", "So! I must say, I am impressed you got here. Team Locket captures Pokaymon from around the world. They're important tools for keeping our criminal enterprise going. I am the leader, Geovanni! For your insolence, you will feel a world of pain!", "ground", "water", terrain);
+const toga = new GymLeader("Toga", "he is the poison-type gym leader", "Fwahahaha! A mere child like you dares to challenge me? That very idea makes me shiver with mirth! Very well, I shall show you true terror as a ninja master! Opponents can't lay a hand on me, as poison brings their steady doom.", "poison", "psychic", spirit);
 
 cratetown.character = mum;
 skyBlue.character = foggy;
@@ -278,9 +304,18 @@ amber.character = hermione;
 olive.character = geovanni;
 blush.character = toga;
 
-const potion = new Item("Potion", "this can be used during a fight to heal your Pokaymon if it looks like it may faint.");
+const potion = new Item("Potion", "this can be used during a fight to heal your Pokaymon if it looks like it may faint");
 
 jade.item = potion;
+
+foggy.badge = waterfall;
+ericar.badge = prism;
+ltemerge.badge = cracking;
+flame.badge = lava;
+block.badge = pebble;
+hermione.badge = quagmire;
+geovanni.badge = terrain;
+toga.badge = spirit;
 
 cratetown.linkCity("east", skyBlue);
 cratetown.linkCity("south", scarlet);
@@ -314,5 +349,7 @@ olive.linkCity("west", blush);
 
 blush.linkCity("north", lead);
 blush.linkCity("east", olive);
+
+
 
 startGame();
